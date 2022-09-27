1077fe01217acf5031b95b9d756d4640c07b0f98
quirama1120 <quirama1120@gmail.com>
2022-09-27 03:35:38 -0500

    practicando con más objetos

--- objetos.js
+++ objetos.js
@@ -34,3 +34,57 @@ const serieUno = new Serie('stranger things', 3,5)
 console.log(peliculaUno.reproducir())
 console.log(peliculaDos.reproducir())
 console.log(serieUno.reproducir())
+
+
+// construir carros como objeto y como clase
+
+const carro = {
+puertas: 5,
+velocidadMaxima: 120,
+acelerar() {
+`el carro está acelerando`
+}
+}
+
+class Carro {
+    constructor(puertas, velocidadMaxima, marca) {
+        this.puertas = puertas;
+        this.velocidadMaxima = velocidadMaxima;
+        this.marca = marca;
+    }
+    acelerar() {
+        return `el carro ${this.marca} alcanzó ${this.velocidadMaxima}`
+    }
+}
+
+class Moto extends Carro {
+constructor(puertas,velocidadMaxima,marca,casco) {
+super(puertas,velocidadMaxima,marca,casco)
+    this.casco = casco;
+}
+acelerar() {
+    return `la ${this.marca} acelera hasta alcanzar ${this.velocidadMaxima}`
+}
+precaucion() {
+    if(this.casco === 0) {
+        return `Mk ojo tienes ${this.casco} cascos`
+    } else {
+       return this.acelerar()
+    }
+}
+}
+const carroUno = new Carro(4, 210,"ferrari")
+const carroDos = new Carro(2,180,"mazda")
+const motoUno = new Moto(0,170,"toyota",2)
+const motoDos = new Moto(0,180,"kawasaky",2)
+console.group();
+console.log(carroUno)
+console.log(carroDos)
+console.log(motoUno)
+console.log(motoDos)
+console.groupEnd()
+
+console.log(carroUno.acelerar())
+console.log(carroDos.acelerar())
+console.log(motoUno.acelerar())
+console.log(motoDos.precaucion())
